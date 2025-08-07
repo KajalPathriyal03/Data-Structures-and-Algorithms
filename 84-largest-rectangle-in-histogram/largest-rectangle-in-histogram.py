91821fff12147c04829e9efcc339c3cff1d0607c
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-08 02:14:29 +0530

    Time: 179 ms (30.42%) | Memory: 30.4 MB (55.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,48 +4,16 @@ class Solution:
         right_small=[-1 for _ in range(len(nums))]
         n=len(nums)
         st=[]
-        for i in range(n):
-            if not st:
-                left_small[i]=0
-                st.append(i)
-            else:
-                print(nums[st[-1]])
-                if nums[i] <= nums[st[-1]]:
-                    while st and nums[st[-1]]>=nums[i]:
-                        st.pop()
-                    if st:
-                        left_small[i]=(st[-1])+1
-                    else:
-                        left_small[i]=0
-                    st.append(i)
-                elif nums[i] >= nums[st[-1]]:
-                    left_small[i]=(st[-1])+1
-                    st.append(i)
-        st=[]
-        for i in range(n-1, -1, -1):
-            if not st:
-                right_small[i]=n-1
-                st.append(i)
-            else:
-                if nums[i]<=nums[st[-1]]:
-                    while st and nums[st[-1]]>=nums[i]:
-                        st.pop()
-                    if st:
-                        right_small[i]=st[-1]-1
-                    else:
-                        right_small[i]=n-1
-                    st.append(i)
-                elif nums[i] >= nums[st[-1]]:
-                    right_small[i]=st[-1]-1
-                    st.append(i)
-        print(left_small, right_small)
-        maxi=-1
-        for i in range(n):
-            ans=nums[i]*(right_small[i]-left_small[i]+1)
-            maxi=max(maxi, ans)
+        maxi=0
+        for i in range(n+1):
+            while st  and (i==n or nums[st[-1]]>=nums[i]):
+                h=nums[st[-1]]
+                st.pop()
+                w=0
+                if not st:
+                    w=i
+                else:
+                    w=i-st[-1]-1
+                maxi=max(maxi, h*w)
+            st.append(i)
         return maxi
-
-
-
-
-
