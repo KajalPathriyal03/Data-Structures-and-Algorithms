fca79c6b7715779853be72183da031908a205657
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-07 22:19:02 +0530

    Time: 15 ms (50.54%) | Memory: 19.2 MB (68.99%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,17 +1,17 @@
 class Solution:
     def trap(self, nums: List[int]) -> int:
         n=len(nums)
-        pref_max=[0 for _ in range(n)]
-        suf_max=[0 for _ in range(n)]
-        pref_max[0]=nums[0]
-        suf_max[n-1]=nums[n-1]
-        for i in range(1, n):
-            pref_max[i]=max(pref_max[i-1], nums[i])
-        for i in range(n-2, -1, -1):
-            suf_max[i]=max(suf_max[i+1], nums[i])
-        
-        ans=0
-        for i in range(1, n-1):
-            if nums[i]<pref_max[i-1] and nums[i]<suf_max[i+1]:
-                ans+=min(pref_max[i-1], suf_max[i+1])-nums[i]
-        return ans
+        l, r=0, n-1
+        l_max, r_max, total=0, 0,0
+        while l<=r:
+            if nums[l]<=nums[r]:
+                l_max=max(l_max, nums[l])
+                if l_max>nums[l]:
+                    total+=(l_max-nums[l])
+                l+=1
+            elif nums[l]>nums[r]:
+                r_max=max(r_max, nums[r])
+                if r_max>nums[r]:
+                    total+=(r_max-nums[r])
+                r-=1
+        return total
