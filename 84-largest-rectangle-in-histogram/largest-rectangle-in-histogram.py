7a9b6c421ff3c3bb0e71261119757934c1e9b5db
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-08 02:19:09 +0530

    Time: 271 ms (8.40%) | Memory: 33.3 MB (37.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,16 +4,33 @@ class Solution:
         right_small=[-1 for _ in range(len(nums))]
         n=len(nums)
         st=[]
-        maxi=0
-        for i in range(n+1):
-            while st  and (i==n or nums[st[-1]]>=nums[i]):
-                h=nums[st[-1]]
+        
+        for i in range(n):
+            while st and nums[i]<=nums[st[-1]]:
                 st.pop()
-                w=0
-                if not st:
-                    w=i
-                else:
-                    w=i-st[-1]-1
-                maxi=max(maxi, h*w)
+            if not st:
+                left_small[i]=0
+            else:
+                left_small[i]=st[-1]+1
             st.append(i)
+        st=[]
+        for i in range(n-1, -1, -1):
+            while st and nums[i]<=nums[st[-1]]:
+                st.pop()
+            if not st:
+                right_small[i]=n-1
+            else:
+                right_small[i]=st[-1]-1
+            st.append(i)
+    
+
+        maxi=-1
+        for i in range(n):
+            ans=nums[i]*(right_small[i]-left_small[i]+1)
+            maxi=max(maxi, ans)
         return maxi
+
+
+
+
+
