9ff42fc4cb828b7ac887db0078ad7c125602bc18
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-10 01:26:33 +0530

    Time: 259 ms (9.89%) | Memory: 33.8 MB (36.25%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,12 +1,11 @@
 class Solution:
     def largestRectangleArea(self, nums: List[int]) -> int:
-        left_small=[-1 for _ in range(len(nums))]
-        right_small=[-1 for _ in range(len(nums))]
         n=len(nums)
+        left_small=[-1 for _ in range(n)]
+        right_small=[-1 for _ in range(n)]
         st=[]
-        
         for i in range(n):
-            while st and nums[i]<=nums[st[-1]]:
+            while st and nums[st[-1]]>=nums[i]:
                 st.pop()
             if not st:
                 left_small[i]=0
@@ -15,22 +14,18 @@ class Solution:
             st.append(i)
         st=[]
         for i in range(n-1, -1, -1):
-            while st and nums[i]<=nums[st[-1]]:
+            while st and nums[st[-1]]>=nums[i]:
                 st.pop()
             if not st:
                 right_small[i]=n-1
             else:
                 right_small[i]=st[-1]-1
             st.append(i)
-    
-
-        maxi=-1
+        maxi=0
         for i in range(n):
-            ans=nums[i]*(right_small[i]-left_small[i]+1)
-            maxi=max(maxi, ans)
+            maxi=max(maxi, nums[i]*(right_small[i]-left_small[i]+1))
         return maxi
 
 
 
-
-
+                                                                                                                                                                                                                    
