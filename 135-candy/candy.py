4d9109f8dc3da524d727f5d8384d048a6ef88208
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-11 21:29:36 +0530

    Time: 23 ms (21.12%) | Memory: 20.2 MB (10.31%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,12 +1,17 @@
 class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        n=len(ratings)
-        ans=[1 for _ in range(n)]
+    def candy(self, nums: List[int]) -> int:
+        n=len(nums)
+        pref=[1 for _ in range(n)]
+        suf=[1 for _ in range(n)]
         for i in range(1, n):
-            if ratings[i]>ratings[i-1]:
-                ans[i]=ans[i-1]+1
-        for i in range(n-2, -1, -1):
-            if ratings[i]>ratings[i+1]:
-                ans[i]=max(ans[i], ans[i+1]+1)
-        return sum(ans)
+            if nums[i]>nums[i-1]:
+                pref[i]=1+pref[i-1]
         
+        for i in range(n-2, -1, -1):
+            if nums[i]>nums[i+1]:
+                suf[i]=max(pref[i], suf[i+1]+1)
+        ans=0
+        for i in range(n):
+            ans+=max(pref[i], suf[i])       
+        # print(pref, suf)
+        return ans
