bbe8000d3ef6db2ac6c51184e0f1617b7773f162
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-12 00:05:14 +0530

    Time: 156 ms (48.39%) | Memory: 41 MB (5.07%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -2,21 +2,35 @@ from sortedcontainers import SortedList
 class MedianFinder:
 
     def __init__(self):
-        self.nums=SortedList()
+        self.max_heap=[]
+        self.min_heap=[]
         
 
     def addNum(self, num: int) -> None:
-        self.nums.add(num)
+        if not self.max_heap or num < -(self.max_heap[0]):
+            heappush(self.max_heap, -num)
+        else:
+            heappush(self.min_heap, num)
+        
+        if abs(len(self.max_heap)-len(self.min_heap))>1:
+            heappush(self.min_heap, -self.max_heap[0])
+            heappop(self.max_heap)
+        elif len(self.max_heap)<len(self.min_heap):
+            heappush(self.max_heap, -self.min_heap[0])
+            heappop(self.min_heap)
         
     def findMedian(self) -> float:
-        n=len(self.nums)
-        # self.nums.sort()
-        if n%2==0:
-            ind=n//2
-            med=(self.nums[ind]+self.nums[ind-1])/2
+        n=len(self.max_heap)
+        m=len(self.min_heap)
+        # print(self.max_heap, self.min_heap)
+        if n==m:
+            first, second=(self.max_heap[0]), (self.min_heap[0])
+            med=((-first)+second)/2
             return med
-        else:
-            return self.nums[n//2]
+        # else
+            # ans=heappop(self.max_heap)
+        return -self.max_heap[0]
+        # return 1.0
 
         
 
