e0661ba809924e7d0404f3b8b2190e047bddf8b3
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-14 01:58:32 +0530

    Time: 83 ms (70.39%) | Memory: 31 MB (38.64%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -3,12 +3,13 @@ class Solution:
         n=len(nums)
         ans=[0 for _ in range(n)]
         stack=[]
-        for i, ele in enumerate(nums):
+        for i in range(n-1, -1, -1):
+            ele=nums[i]
             while stack and nums[stack[-1]]<=ele:
-                top=stack.pop()
-                ans[top]+=1
+                stack.pop()
+                ans[i]+=1
             if stack:
-                ans[stack[-1]]+=1
+                ans[i]+=1
             stack.append(i)
         return ans 
         
