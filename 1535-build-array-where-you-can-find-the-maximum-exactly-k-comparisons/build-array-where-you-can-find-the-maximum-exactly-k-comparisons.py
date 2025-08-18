8622e840e9d1827dab029eb27ee49c5aadf9e2d2
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-08-19 00:46:33 +0530

    Time: 2563 ms (38.31%) | Memory: 60.6 MB (9.74%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,23 +1,20 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
         mod=10**9+7
-        dp={}
+        @cache
         def solve(ind, search_cost, max_so_far):
             if ind==n:
                 if search_cost==k:
                     return 1
                 else:
                     return 0
-            if (ind, search_cost, max_so_far) in dp:
-                return dp[(ind, search_cost, max_so_far)]
             res=0
             for i in range(1, m+1):
                 if i>max_so_far:
                     res=(res+solve(ind+1, search_cost+1, i))%mod
                 else:
                     res=(res+solve(ind+1, search_cost, max_so_far))%mod
-            dp[(ind, search_cost, max_so_far)]= res%mod
-            return dp[(ind, search_cost, max_so_far)]
+            return res%mod
 
         return solve(0, 0, -1)
         
