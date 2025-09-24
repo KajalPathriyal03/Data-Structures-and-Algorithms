67702836aae5662e0a1d7e76883113b70ddd9db6
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-09-24 18:27:45 +0530

    Time: 290 ms (5.07%) | Memory: 18.2 MB (8.14%) - LeetSync

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -1,31 +1,28 @@
 class Solution:
     def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
-        zeroes, ones, twos=0, 0,0
-        n=len(grid)
-        ways=[[0, 1], [0,2], [1, 0], [1, 2], [2, 0], [2, 1]]
-        dis=[[-1 for _ in range(n)] for _ in range(n)]
-        mini=float('inf')
+        n, m=len(grid), len(grid[0])
+        ways=[[0, 1], [0, 2], [1,0], [1, 2],[2, 0], [2,1]]
+        dis=[[-1 for _ in range(len(grid[0]))]for _ in range(len(grid))]
+        mini=n*n
         for p, s in ways:
-            mid=n//2
-            cnt=0
-            for i in range(n):
-                for j in range(n):
-                    dis[i][j]=s
-                    if i<mid:
-                        if i==j:  
-                            dis[i][j]=p
-                        elif j==n-i-1:
-                            dis[i][j]=p
+            ans=0
+            for row in range(n):
+                for col in range(m):
+                    dis[row][col]=p
+                    mid=n//2
+                    if row<mid:
+                        if row==col:
+                            dis[row][col]=s
+                        elif col==n-row-1:
+                            dis[row][col]=s
                     else:
-                        if j==mid:
-                            dis[i][j]=p
-            for i in range(n):
-                for j in range(n):
-                    if dis[i][j]!=grid[i][j]:
-                        cnt+=1
-            mini=min(mini, cnt)
-            
-        return mini                    
-                        
-            
-        
+                        if col==mid:
+                            dis[row][col]=s
+            print(dis)
+            for row in range(n):
+                for col in range(m):
+                    if dis[row][col]!=grid[row][col]:
+                        ans+=1
+            mini=min(mini, ans)
+                      
+        return mini 
