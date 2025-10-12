fb916245786f39f8a07867407398d3ac0df1a53d
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-10-12 23:41:42 +0530

    Time: 187 ms (49.67%) | Memory: 23.7 MB (12.63%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,32 +1,25 @@
 class Solution:
     def totalFruit(self, nums: List[int]) -> int:
-        n=len(nums)
-        l, r=0, 0
-        mp={}
-        maxi=0
-        if n==1: return 1
+        ''' [3,3,3,1,2,1,1,2,3,3,4]
+        l=3
+        r=7
+        ans=5
+        st=[1 2
+        if len(st)>2: discard(nums(l)): l+=1
+        '''
+        l, r, n=0, 0, len(nums)
+        mp=defaultdict(int)
+        ans=0
         while r<n:
-            ele=nums[r]
-            if len(mp)==3:
+            mp[nums[r]]+=1
+            while len(mp)>2:
                 mp[nums[l]]-=1
                 if mp[nums[l]]==0:
                     del mp[nums[l]]
                 l+=1
-                continue
-            else:
-                if ele not in mp:
-                    mp[ele]=1
-                else:
-                    mp[ele]+=1
-                r+=1
-                cs=0
-                if len(mp)<=2:
-                    for ele, cnt in mp.items():
-                        cs+=cnt
-                    maxi=max(maxi, cs)
+            ans =max(ans, r-l+1)
+            # print(ans, mp)
+            r+=1
+        return ans 
+            
 
-        return maxi
-        
-
-
-        
