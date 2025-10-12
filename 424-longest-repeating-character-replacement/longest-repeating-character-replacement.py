0e6479637f59a74d8aaeee471ec500cf6d547d21
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-10-13 01:43:51 +0530

    Time: 296 ms (9.34%) | Memory: 18.1 MB (23.54%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -8,12 +8,17 @@ class Solution:
         while r<n:
             mp[s[r]]+=1
             maxFreq=max(maxFreq, mp[s[r]])
-            changes=(r-l+1)-maxFreq
-            if changes>k:
+            # changes=(r-l+1)-maxFreq
+            while (r-l+1)-maxFreq>k:
+                print(l)
                 mp[s[l]]-=1
+                maxFreq=0
                 if mp[s[l]]==0:
                     del mp[s[l]]
+                for ele, val in mp.items():
+                    maxFreq=max(maxFreq, val)
                 l+=1
+            # if (r-l+1)-maxFreq<=k:
             ans=max(ans, r-l+1)
             # print(l, r, ans)
             r+=1
