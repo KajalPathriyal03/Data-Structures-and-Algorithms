aa30395fa5e889dff7c1cb11b07306ed172467ad
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-10-19 16:24:38 +0530

    Time: 42 ms (50.72%) | Memory: 18.7 MB (19.03%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,18 +9,32 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
-        
-        ans={}
-        dummy=head
-        while dummy:
-            ans[dummy]=Node(dummy.val)
-            dummy=dummy.next
-        # print(ans)
-        dummy=head
-        while dummy:
-            ans[dummy].next=ans.get(dummy.next)
-            ans[dummy].random=ans.get(dummy.random)
-            dummy=dummy.next
-        return ans[head]
+        #1. insert copy nodes in between
+        #2 connect random pointers 
+        #3 connect next pointers 
+
+        if not head: return None 
+        tmp=head 
+        while tmp:
+            copy=Node(tmp.val)
+            copy.next=tmp.next
+            tmp.next=copy
+            tmp=tmp.next.next
+        tmp=head 
+        while tmp:
+            copy=tmp.next
+            if not tmp.random:
+                copy.random=None
+            else:
+                copy.random=tmp.random.next
+            tmp=tmp.next.next
+
+        dummy=Node(-1)
+        res=dummy
+        tmp=head 
+        while tmp:
+            res.next=tmp.next
+            tmp.next=tmp.next.next
+            res=res.next
+            tmp=tmp.next
+        return dummy.next
