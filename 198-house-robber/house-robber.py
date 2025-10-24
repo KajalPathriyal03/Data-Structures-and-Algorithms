9138003e549f4cbd1f03ad26d198249cca01e9a0
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-10-24 13:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.35%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,11 @@
 class Solution:
-    def solve(self, nums, i):
-        if i>=len(nums):
-            return 0
-        if i in self.dp:
-            return self.dp[i]
-        steal=nums[i]+self.solve(nums, i+2)
-        skip=0+self.solve(nums, i+1)
-        self.dp[i]= max(steal, skip)
-        return self.dp[i]
     def rob(self, nums: List[int]) -> int:
-        self.dp={}
-        self.ans=self.solve(nums, 0)
-        return self.ans
+        # dp[i]=maximum stolen money till house i
+        dp=[-1 for _ in range(len(nums)+1)]
+        dp[0]=0
+        dp[1]=nums[0]
+        for i in range(1, len(nums)):
+            dp[i+1]=max(dp[i-1]+nums[i], dp[i])
+        # print(dp)
+        return dp[len(nums)]
         
