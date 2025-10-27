96fb13f033cfc0ece753836837085f83554057c1
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-10-27 14:34:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (95.05%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,15 +1,10 @@
 class Solution:
-    def findClosestElements(self, nums, k, x):
-        h = bisect.bisect_left(nums, x)
-        l = h - 1
-        print(nums,h, l)
-        while k > 0:
-            if l < 0:
-                h += 1
-            elif h >= len(nums) or x - nums[l] <= nums[h] - x:
-                l -= 1
+    def findClosestElements(self, A, k, x):
+        left, right = 0, len(A) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - A[mid] > A[mid + k] - x:
+                left = mid + 1
             else:
-                h += 1
-            k -= 1
-        return nums[l + 1:h]
-        
+                right = mid
+        return A[left:left + k]
