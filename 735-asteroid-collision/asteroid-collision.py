189f40685f021badb6918983ad6fa2e36c434162
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-11-02 21:05:38 +0530

    Time: 8 ms (27.18%) | Memory: 18.8 MB (60.71%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,18 +1,21 @@
 class Solution:
     def asteroidCollision(self, nums: List[int]) -> List[int]:
+        ''' 1. if both are negetive/positive -- never meet
+            2. if opposite direction -- explode 
+        '''
         n=len(nums)
         st=[]
         ans=[]
         for i in range(n):
-            while st and nums[i]<0<st[-1]:
-                if -nums[i]>st[-1]:
-                    st.pop()
-                    continue
-                elif -nums[i]==st[-1]:
-                    st.pop()
-                break
-            else:
+            if nums[i]>0:
                 st.append(nums[i])
+            else:
+                while st and st[-1]>0 and abs(nums[i])>st[-1]:
+                    st.pop()
+                if st and abs(nums[i])==st[-1]:
+                    st.pop()
+                elif not st or st[-1]<0:
+                    st.append(nums[i])
         return st
 
 
