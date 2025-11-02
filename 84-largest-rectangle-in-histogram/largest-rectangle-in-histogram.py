4de1ff999ba9d431b83822a74a5e03ff793be8c8
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-11-02 20:40:02 +0530

    Time: 167 ms (40.66%) | Memory: 29.3 MB (83.13%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,36 +1,19 @@
 class Solution:
     def largestRectangleArea(self, nums: List[int]) -> int:
-        n=len(nums)
-        left_small=[-1 for _ in range(n)]
-        st=[]
-        for i in range(n):
-            while st and nums[st[-1]]>=nums[i]:
-                st.pop()
-            if not st:
-                left_small[i]=-1
-            else:
-                left_small[i]=st[-1]
-            st.append(i)
-        right_small=[n for _ in range(n)]
-        st=[]
-        for i in range(n-1, -1, -1):
-            while st and nums[st[-1]]>=nums[i]:
-                st.pop()
-            if not st:
-                right_small[i]=n
-            else:
-                right_small[i]=st[-1]
-            st.append(i)
-        print(right_small)
-
         ans=0
-        for i in range(n):
-            l=left_small[i]+1
-            r=right_small[i]-1
-            ans=max(ans, nums[i]*(r-l+1))
+        st=[]
+        for i in range(len(nums)+1):
+            while st and (i==len(nums) or nums[st[-1]]>=nums[i]):
+                height=nums[st.pop()]
+                wt =0
+                if not st:
+                    wt=i
+                else:
+                    wt=i-st[-1]-1
+                # l=prev+1
+                # r=i-1
+                ans=max(ans, height*wt)
+            st.append(i)
         return ans 
-
-
-
-
+            
         
