d3e545c095abba06925a039fbc6d37efcce31ad6
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-04 21:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,9 +1,20 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def rec(self, n):
+        if n==0:
+            return 1
+        if n<0:
+            return 0
+        add_1=self.rec(n-1)
+        add_2=self.rec(n-2)
+        return add_1+add_2
 
-        if n<=1: return 1
-        f1, f2=1, 1
+    def climbStairs(self, n: int) -> int:
+        f,s=1,1
+        if n==0 or n==1: return f
         for i in range(2, n+1):
-            f1, f2=f2, f1+f2
-        return f2
+            cur=f+s
+            f=s
+            s=cur 
+        return s
+
         
