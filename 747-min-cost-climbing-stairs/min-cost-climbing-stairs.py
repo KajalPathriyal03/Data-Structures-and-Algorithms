f3b3e6edd0a786f248bd46802233238965d0c56d
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-04 22:28:13 +0530

    Time: 3 ms (73.92%) | Memory: 17.8 MB (62.79%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,11 +1,23 @@
+''' as it depends only on last two points so we dont need any dp here only two variables are enogh'''
 class Solution:
-    def minCostClimbingStairs(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[float('inf') for _ in range(n+1)]
-        dp[n]=0
-        dp[n-1]=nums[n-1]
-        for i in range(n-2, -1, -1):
-            dp[i]=min(dp[i+1], dp[i+2])+nums[i]
-        # print(dp)
-        return min(dp[0], dp[1])
+    def rec(self, ind, cost, cur_cost):
+        if ind >= len(cost):
+            self.mini=min(self.mini, cur_cost)
+            return 
+        self.rec(ind+1, cost, cur_cost+cost[ind])
+        self.rec(ind+2, cost, cur_cost+cost[ind])
         
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n=len(cost)
+        self.dp={} # till ith point what is the minimum cost 
+        f, s=cost[0],cost[1]
+        if n==0: return 0
+        if n==1: return f
+        if n==2: return min(f, s)
+
+        for i in range(2, n):
+            cur=cost[i]+min(f, s)
+            f=s
+            s=cur 
+        return min(f, s)
