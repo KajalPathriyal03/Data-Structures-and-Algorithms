bc84cb8e783c55eeb7366a440dbca226eba7ba5f
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-05 14:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,20 +1,19 @@
 class Solution:
-    def rec(self, ind, nums):
-        if ind >= len(nums):
-            return 0
-        if ind in self.dp:
-            return self.dp[ind]
-        not_take=self.rec((ind+1), nums)
-        take=nums[ind]+self.rec((ind+2), nums)
-        self.dp[ind]= max(take, not_take)
-        return self.dp[ind]
+    def rec(self, nums):
+        prev=0
+        prev2=0
+        for ele in nums:
+            take=ele+prev2
+            not_take=prev
+            cur=max(take, not_take)
+            prev2=prev
+            prev=cur 
+        return prev
     def rob(self, nums: List[int]) -> int:
-        self.dp={}
         if len(nums)==1:
             return nums[0]
         arr=nums
-        front=self.rec(0, arr[:len(nums)-1])
-        self.dp={}
-        back=self.rec(0, arr[1:])
+        front=self.rec(arr[:len(nums)-1])
+        back=self.rec(arr[1:])
         return max(front, back)
         
