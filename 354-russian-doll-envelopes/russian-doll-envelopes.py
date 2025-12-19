f46b8575bc1a591140883c4094a0952152a781d7
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-19 12:44:57 +0530

    Time: 137 ms (72.61%) | Memory: 53 MB (33.88%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,17 @@
+import bisect
+class Solution:
+    def maxEnvelopes(self, nums: List[List[int]]) -> int:
+        nums=sorted(nums, key=lambda x: (x[0], -x[1]))
+        lis=[]
+        lis.append(nums[0][1])
+        for i in range(1, len(nums)):
+            ind=bisect.bisect_left(lis, nums[i][1])
+            if ind==len(lis):
+                lis.append(nums[i][1])
+            else:
+                lis[ind]=nums[i][1]
+        return len(lis)
+
+
+
+        
