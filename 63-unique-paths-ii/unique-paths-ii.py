048c55a72c7c731340399fbf651b4b7e7b6da873
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-19 13:02:45 +0530

    Time: 2 ms (28.71%) | Memory: 17.4 MB (100.00%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,16 +1,14 @@
 class Solution:
     def rec(self, i,j, grid, vis):
-        if i<0 or j<0 or i>=len(grid) or j >= len(grid[0]) or (i, j) in vis or grid[i][j]==1:
+        if i<0 or j<0 or i>=len(grid) or j >= len(grid[0])or grid[i][j]==1:
             return 0
         if i==len(grid)-1 and j==len(grid[0])-1:
             return 1
         if (i, j) in self.dp:
             return self.dp[(i, j)]
 
-        vis.add((i, j))
         right=self.rec(i, j+1, grid, vis)
         down=self.rec(i+1, j, grid, vis)
-        vis.remove((i, j))
         self.dp[(i, j)]= right+down
         return self.dp[(i, j)]
 
