35a8f45ad485f3b1ecaef416f8c5b821dac1403e
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-26 12:36:00 +0530

    Time: 936 ms (45.68%) | Memory: 95.8 MB (25.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,27 +1,22 @@
 class Solution:
-    def rec(self, i, j, s1, s2):
-        if i>=len(s1) or j>=len(s2):
+    def rec(self, i, j, s):
+        if i>j:
             return 0
-        if (i, j) in self.dp:
+        if i==j:
+            return 1
+        if (i,j) in self.dp:
             return self.dp[(i, j)]
-        ans=0
-        if s1[i]==s2[j]:
-            ans=1+self.rec(i+1, j+1, s1, s2)
+        ans = 0
+        if s[i]==s[j]:
+            ans=2+self.rec(i+1, j-1, s)
         else:
-            one=self.rec(i, j+1, s1, s2)
-            two=self.rec(i+1, j, s1, s2)
-            ans=max(one, two)
-        
+            one=self.rec(i+1, j, s)
+            two=self.rec(i,j-1, s)
+            ans=max(one,two)
         self.dp[(i, j)]= ans 
         return self.dp[(i, j)]
 
-    def lcs(self, s, target):
-        self.dp={}
-        ans=self.rec(0, 0, s, target)
-        return ans 
-        
     def longestPalindromeSubseq(self, s: str) -> int:
-        target=s[::-1]
-        return self.lcs(s, target)
-
+        self.dp={}
+        return self.rec(0, len(s)-1, s)
         
