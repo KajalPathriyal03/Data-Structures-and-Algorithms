bec1198dddf353be7c704d12d3500c7a667daaf4
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2025-12-26 23:25:11 +0530

    Time: 45 ms (53.51%) | Memory: 18 MB (61.65%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,23 +1,23 @@
 class Solution:
-    def rec(self, i, j, k, s1, s2, s3):
-        if i==len(s1) and j==len(s2) and k==len(s3):
+    def rec(self, i, j, s1, s2, s3):
+        if i==len(s1) and j==len(s2):
             return True 
-        if k==len(s3):
+        if (i+j)==len(s3):
             return False
-        if (i, j, k) in self.dp:
-            return self.dp[(i, j, k)]
+        if (i, j) in self.dp:
+            return self.dp[(i, j)]
         ans=False
-        if i<len(s1) and s1[i]==s3[k]:
-            ans=self.rec(i+1, j, k+1, s1, s2, s3)
+        if i<len(s1) and s1[i]==s3[(i+j)]:
+            ans=self.rec(i+1, j, s1, s2, s3)
         if ans==True:
             return True
-        if j<len(s2) and s2[j]==s3[k]:
-            ans=self.rec(i, j+1, k+1, s1, s2, s3)
-        self.dp[(i, j, k)]= ans
-        return self.dp[(i, j, k)]
+        if j<len(s2) and s2[j]==s3[(i+j)]:
+            ans=self.rec(i, j+1, s1, s2, s3)
+        self.dp[(i, j)]= ans
+        return self.dp[(i, j)]
 
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         self.dp={}
         if len(s1)+len(s2)<len(s3): return False
-        return self.rec(0, 0, 0, s1, s2, s3)
+        return self.rec(0, 0, s1, s2, s3)
         
