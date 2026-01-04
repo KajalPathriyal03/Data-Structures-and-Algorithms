be91b90f043148d49d33c7b517d88ee0b74b1105
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-04 13:53:28 +0530

    Time: 3 ms (80.40%) | Memory: 18.5 MB (94.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,18 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l, r=0, len(nums)-1
-        for i in range(len(nums)):
+        i=0
+        while l<=r:
             if nums[i]!=0:
                 nums[l]=nums[i]
                 l+=1
-        while l<len(nums):
-            nums[l]=0
-            l+=1
+            else:
+                r-=1
+            i+=1
+        r=r+1
+        while r<len(nums):
+            nums[r]=0
+            r+=1
+
         return nums 
         
