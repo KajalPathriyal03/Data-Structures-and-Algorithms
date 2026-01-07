802456bce348b9a0ee0a03b04a0fb8693578760f
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-07 16:12:19 +0530

    Time: 64 ms (15.85%) | Memory: 31.1 MB (26.66%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,25 +1,15 @@
-from sortedcontainers import  SortedSet
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n=len(nums)
-        contains_one=False 
+        i=0
+        while i<len(nums):
+            ind=nums[i]-1
+            if nums[i]>=1 and nums[i]<=len(nums) and nums[i]!=nums[ind]:
+                nums[i], nums[ind]= nums[ind], nums[i]
+            else:
+                i+=1
+
         for i in range(len(nums)):
-            if nums[i]==1:
-                contains_one=True
-            if nums[i]<=0:
-                nums[i]=1
-            if nums[i]>n:
-                nums[i]=1
-        if not contains_one:
-            return 1
-        
-        for i in range(n):
-            num=abs(nums[i])
-            ind=num-1
-            if nums[ind]<0:
-                continue
-            nums[ind]*=-1
-        for i in range(n):
-            if nums[i]>0:
+            if nums[i]!=i+1:
                 return i+1
-        return n+1
+        return len(nums)+1
+        
