5aa19c15f7bbc58b90d296c04d975ea1f889787a
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-08 16:42:58 +0530

    Time: 2 ms (97.39%) | Memory: 20.2 MB (5.78%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,16 +1,11 @@
 class Solution:
-    def cntOnes(self, n):
-        cnt=0
-        while n:
-            n=n&(n-1)
-            cnt+=1
-        return cnt
     def countBits(self, n: int) -> List[int]:
-        ans=[]
-        ans.append(0)
+        dp=[0 for _ in range(n+1)]
+        poweroftwo=1
         for i in range(1, n+1):
-            cnt=self.cntOnes(i)
-            ans.append(cnt)
-        return ans
+            if poweroftwo*2==i:
+                poweroftwo=i
+            dp[i]=1+dp[i-poweroftwo]
+        return dp
 
         
