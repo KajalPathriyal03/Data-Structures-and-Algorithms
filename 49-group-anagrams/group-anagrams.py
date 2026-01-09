e158ffa4b22b2006d423efb245e5b04439609552
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-09 21:40:37 +0530

    Time: 15 ms (50.35%) | Memory: 22.2 MB (26.06%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''Actually basic idea of solution 2 is the same as solution 1, but difference between solution 1 and solution 2 is that we don't have to sort each character.
+        mp=defaultdict(list)
+        for ele in strs:
+            s="".join(sorted(ele))
+            mp[s].append(ele)
 
-First of all, we create array with 26 length. 26 is number of alphablets.'''
-        ans=defaultdict(list)
-        for s in strs:
-            cnt=[0]*26
-            for c in s:
-                cnt[ord(c)-ord('a')]+=1
-            ans[tuple(cnt)].append(s)
-        return list(ans.values())
-
+        ans=[]
+        for val in mp.values():
+            ans.append(val)
+        return ans
         
