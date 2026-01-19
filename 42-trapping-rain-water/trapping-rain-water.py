448a013c744898dab5a68f2805cf1bed31d873c7
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-19 14:27:17 +0530

    Time: 34 ms (11.52%) | Memory: 21.3 MB (7.07%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,17 +1,19 @@
 class Solution:
     def trap(self, nums: List[int]) -> int:
-        n=len(nums)
-        l, r=0, n-1
-        l_max, r_max, total=0, 0,0
-        while l<=r:
-            if nums[l]<=nums[r]:
-                l_max=max(l_max, nums[l])
-                if l_max>nums[l]:
-                    total+=(l_max-nums[l])
-                l+=1
-            elif nums[l]>nums[r]:
-                r_max=max(r_max, nums[r])
-                if r_max>nums[r]:
-                    total+=(r_max-nums[r])
-                r-=1
-        return total
+        pref, suf=[0 for _ in range(len(nums))], [0 for _ in range(len(nums))]
+        for i in range(len(nums)):
+            if i==0:
+                pref[i]=nums[i]
+                suf[len(nums)-i-1]=nums[len(nums)-i-1]
+            else:
+                pref[i]=max(pref[i-1], nums[i])
+                suf[len(nums)-i-1] = max(suf[len(nums)-i], nums[len(nums)-i-1]) 
+        ans=0
+        for i in range(1, len(nums)-1):
+            left_max=pref[i-1]
+            right_max=suf[i+1]
+            if min(left_max, right_max)-nums[i]>0:
+                ans+=min(left_max, right_max)-nums[i]
+        return ans 
+                    
+        
