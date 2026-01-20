27a250d08599d63c8dd01a011b75b433914c1897
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-20 22:14:49 +0530

    Time: 43 ms (60.15%) | Memory: 19.7 MB (19.47%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,19 +1,19 @@
-class Solution:
-    def anagram(self, s):
-        nums=list(s)
-        nums.sort()
-        return "".join(nums)
-        
+class Solution:  
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s): return []
+        if len(p)>len(s): 
+            return []
         mp=defaultdict(int)
+        dp=defaultdict(int)
+
         for ele in p:
             mp[ele]+=1
+
         ans=[]
         k=len(p)
-        dp=defaultdict(int)
+
         for i in range(k):
             dp[s[i]]+=1
+
         r=k
         while r<len(s):
             if mp==dp:
@@ -25,6 +25,7 @@ class Solution:
             r+=1
         if mp==dp:
             ans.append(r-k)
+
         return ans 
         
         
