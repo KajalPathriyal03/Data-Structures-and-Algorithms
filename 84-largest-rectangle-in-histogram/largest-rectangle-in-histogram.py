b00de3a842c0f2a474dd90d4065ea9342de38446
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-22 22:46:55 +0530

    Time: 177 ms (25.50%) | Memory: 31.7 MB (38.45%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,25 +1,18 @@
 class Solution:
     def largestRectangleArea(self, nums: List[int]) -> int:
         left_small=[-1 for _ in range(len(nums))]
-        right_small=[len(nums) for _ in range(len(nums))]
+        right_small=[len(nums) for _  in range(len(nums))]
         stack=[]
         stack.append(0)
         for i in range(1, len(nums)):
             while stack and nums[stack[-1]]>=nums[i]:
+                right_small[stack[-1]]=i
                 stack.pop()
             if stack:
                 left_small[i]=stack[-1]
             stack.append(i)
-        stack=[]
-        stack.append(len(nums)-1)
-        for i in range(len(nums)-2,-1, -1):
-            while stack and nums[stack[-1]]>=nums[i]:
-                stack.pop()
-            if stack:
-                right_small[i]=stack[-1]
-            stack.append(i)
-        ans=-1
+        ans=0
         for i in range(len(nums)):
             distance=right_small[i]-left_small[i]-1
             ans=max(ans, distance*nums[i])
-        return ans 
+        return ans
