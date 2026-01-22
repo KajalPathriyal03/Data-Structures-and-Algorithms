21587dbf7dcfd810023943c8987b68099a3f86ba
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-22 22:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (8.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,34 @@
 class Solution:
+    def lower_bound(self, nums, target):
+        l, r=0, len(nums)-1
+        ans=-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                ans=mid
+                r=mid-1
+            elif nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+        return ans 
+    
+    def upper_bound(self, nums, target):
+        l, r=0, len(nums)-1
+        ans=-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                ans=mid
+                l=mid+1
+            elif nums[mid]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        def lowerBound(nums, target):
-            l, r=0, n-1
-            ans=float('inf')
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]==target:
-                    ans=mid
-                    r=mid-1
-                elif nums[mid]<target:
-                    l=mid+1
-                else:
-                    r=mid-1
-            if ans == float('inf'):
-                return -1
-            return ans 
-        ans1=lowerBound(nums, target)
-        def upperBound(nums, target):
-            l, r=0,n-1
-            ans=float('inf')
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]==target:
-                    ans=mid
-                    l=mid+1
-                elif nums[mid]<target:
-                    l=mid+1
-                else:
-                    r=mid-1
-            if ans == float('inf'):
-                return -1
-            return ans 
-        ans2=upperBound(nums, target)
-        return [ans1, ans2]
-
-
-
-            
+        a=self.lower_bound(nums, target)
+        b=self.upper_bound(nums, target)
+        return [a, b]
         
