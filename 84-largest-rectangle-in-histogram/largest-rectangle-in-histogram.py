ecfa31e77bbbbfeffa1994592218fda7c3e0e1e2
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-01-22 22:18:03 +0530

    Time: 227 ms (13.74%) | Memory: 32 MB (37.09%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,19 +1,25 @@
 class Solution:
     def largestRectangleArea(self, nums: List[int]) -> int:
-        ans=0
-        st=[]
-        for i in range(len(nums)+1):
-            while st and (i==len(nums) or nums[st[-1]]>=nums[i]):
-                height=nums[st.pop()]
-                wt =0
-                if not st:
-                    wt=i
-                else:
-                    wt=i-st[-1]-1
-                # l=prev+1
-                # r=i-1
-                ans=max(ans, height*wt)
-            st.append(i)
+        left_small=[-1 for _ in range(len(nums))]
+        right_small=[len(nums) for _ in range(len(nums))]
+        stack=[]
+        stack.append(0)
+        for i in range(1, len(nums)):
+            while stack and nums[stack[-1]]>=nums[i]:
+                stack.pop()
+            if stack:
+                left_small[i]=stack[-1]
+            stack.append(i)
+        stack=[]
+        stack.append(len(nums)-1)
+        for i in range(len(nums)-2,-1, -1):
+            while stack and nums[stack[-1]]>=nums[i]:
+                stack.pop()
+            if stack:
+                right_small[i]=stack[-1]
+            stack.append(i)
+        ans=-1
+        for i in range(len(nums)):
+            distance=right_small[i]-left_small[i]-1
+            ans=max(ans, distance*nums[i])
         return ans 
-            
-        
