ed3cc66378c592074a66f32ca04837597e86cc48
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-04 21:57:35 +0530

    Time: 11 ms (60.85%) | Memory: 22.7 MB (45.73%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,35 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists)==0: return None
-        def merge_lists(l1, l2):
-            node = ListNode()
-            ans=node
-            while l1 and l2:
-                if l1.val>l2.val:
-                    node.next=l2
-                    l2=l2.next
-                else:
-                    node.next=l1
-                    l1=l1.next
-                node=node.next
-            if l1:
-                node.next=l1
+    def mergeTwo(self, i, j):
+        node=ListNode()
+        ans=node
+        while i and j:
+            if i.val > j.val:
+                node.next=j
+                j=j.next
             else:
-                node.next=l2
-            return ans.next
+                node.next=i
+                i=i.next
+            node=node.next
+        if i:
+            node.next=i
+        else:
+            node.next=j
 
-        
+        return ans.next
+            
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+        ans=[]
         while len(lists)>1:
-            tmp=[]
+            merge=[]
             for i in range(0, len(lists), 2):
                 l1=lists[i]
-                l2=lists[i+1] if i+1<len(lists) else None
-                tmp.append(merge_lists(l1, l2))
-            lists=tmp
-        # print(lists[1])
+                l2=None
+                if i+1<len(lists):
+                    l2=lists[i+1]
+                merge.append(self.mergeTwo(l1, l2))
+            
+            lists=merge
         return lists[0]
-        
-
-        
