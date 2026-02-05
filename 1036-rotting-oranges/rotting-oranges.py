7533fb859c99fe9b9e98e406c6bad8f9c572729b
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-05 13:29:56 +0530

    Time: 3 ms (78.85%) | Memory: 19.4 MB (41.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,29 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n,m=len(grid), len(grid[0])
+        rows, cols=len(grid), len(grid[0])
         queue=deque()
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]==2:
+                    queue.append((row, col, 0))
         ans=0
-        print(queue)
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]==2:
-                    queue.append((i, j, 0))
-
         while queue:
-            r, c, cost=queue.popleft()
-            ans=cost
-            directions=[[1, 0], [-1, 0], [0, 1], [0, -1]]
+            row, col, time=queue.popleft()
+            ans=time 
+            directions=[[1, 0], [-1, 0], [0,1], [0, -1]]
             for x, y in directions:
-                newR, newC=r+x, c+y
-                if newR>=0 and newC>=0 and newR<n and newC<m and grid[newR][newC]==1:
-                    grid[newR][newC]=2
-                    queue.append((newR, newC, cost+1)) 
-                    
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]==1:
-                    return -1
+                newx=row+x
+                newy=col+y
+                if newx>=0 and newx<rows and newy>=0 and newy<cols and grid[newx][newy]==1:
+                    grid[newx][newy]=2
+                    queue.append((newx, newy, time+1))
 
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]==1:
+                    return -1
+        
         return ans 
 
         
