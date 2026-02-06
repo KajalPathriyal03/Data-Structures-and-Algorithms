98a692caedc1e86b57b3429fbc7f794bf82d792b
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-06 13:39:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (37.00%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,19 +1,24 @@
 class Solution:
-    def rec(self, nums):
-        prev=0
-        prev2=0
-        for ele in nums:
-            take=ele+prev2
-            not_take=prev
-            cur=max(take, not_take)
-            prev2=prev
-            prev=cur 
-        return prev
+    def dfs(self, ind, nums):
+        if ind>=len(nums):
+            return 0
+        if ind in self.dp:
+            return self.dp[ind]
+        not_take=self.dfs(ind+1, nums)
+        take =nums[ind]+self.dfs(ind+2, nums)
+    
+        self.dp[ind]= max(not_take, take)
+        return self.dp[ind]
+        
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        arr=nums
-        front=self.rec(arr[:len(nums)-1])
-        back=self.rec(arr[1:])
-        return max(front, back)
+    
+        self.dp={}
+        ans1, ans2=0, 0
+        n=len(nums)
+        if n==1: return nums[0] #edge case 
+
+        ans1=self.dfs(0, nums[:n-1])
+        self.dp={}
+        ans2=self.dfs(0, nums[1:])
+        return max(ans1, ans2)
         
