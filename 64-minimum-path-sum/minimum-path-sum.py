87faf7592956ea66acda44d489b44a1a6120643e
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-06 19:22:00 +0530

    Time: 56 ms (6.99%) | Memory: 25.8 MB (10.77%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,18 +1,20 @@
 class Solution:
-    def rec(self, i, j, grid):
-        if i<0 or j<0 or i>=len(grid) or j >= len(grid[0]):
+    def dfs(self, row, col, grid):
+        if row>=len(grid) or col>=len(grid[0]):
             return float('inf')
-        if i==len(grid)-1 and j==len(grid[0])-1:
-            return grid[i][j]
-        if (i, j) in self.dp:
-            return self.dp[(i, j)]
 
-        right=self.rec(i, j+1, grid)
-        down=self.rec(i+1, j, grid)
-        self.dp[(i, j)]= grid[i][j]+min(right,down)
-        return self.dp[(i, j)]
+        if row==len(grid)-1 and col==len(grid[0])-1:
+            return grid[row][col]
 
+        if (row, col) in self.dp:
+            return self.dp[(row, col)]
+
+        right=grid[row][col]+self.dfs(row, col+1, grid)
+        down=grid[row][col]+self.dfs(row+1, col, grid)
+
+        self.dp[(row, col)]= min(right, down)
+        return self.dp[(row, col)]
     def minPathSum(self, grid: List[List[int]]) -> int:
         self.dp={}
-        ans=self.rec(0, 0, grid)
-        return ans 
+        return self.dfs(0, 0, grid)
+        
