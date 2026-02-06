49ff823c90c3b4abfda68bb9e0e0271f6236f0c4
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-06 16:27:27 +0530

    Time: 13 ms (72.88%) | Memory: 19.8 MB (13.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,8 +1,19 @@
 class Solution:
+    def lower_bound(self, nums, target):
+        ans=len(nums)
+        l, r=0, len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]>=target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans 
     def lengthOfLIS(self, nums: List[int]) -> int:
         ans=[]
         for i in range(len(nums)):
-            ind=bisect.bisect_left(ans, nums[i])
+            ind=self.lower_bound(ans, nums[i])
             if ind==len(ans):
                 ans.append(nums[i])
             
