d32aa2a6a327216ad8cf0c0ade474a4714b7c652
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-07 20:32:36 +0530

    Time: 191 ms (44.91%) | Memory: 35.5 MB (25.55%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,20 +1,22 @@
-from sortedcontainers import SortedList
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n=len(nums)
-        queue=deque()
         ans=[]
-        for r in range(n):
-            while queue and nums[queue[-1]]<=nums[r]:
-                queue.pop()
-            
-            queue.append(r)
-            if queue[0]==r-k:
+        queue=deque()
+        for i in range(len(nums)):
+            if queue and i-queue[0]+1>k:
                 queue.popleft()
-            if r>=k-1:
+
+            while queue and nums[queue[-1]]<=nums[i]:
+                queue.pop()
+
+            queue.append(i)
+
+            if i>=k-1:
                 ans.append(nums[queue[0]])
+
         return ans 
 
+
+
             
-            
-            
+        
