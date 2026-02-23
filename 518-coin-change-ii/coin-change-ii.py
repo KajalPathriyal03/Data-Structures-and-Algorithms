33a5aded0f7c887a205a2fa3a76b30d08d618e9f
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-02-23 20:46:39 +0530

    Time: 1560 ms (10.81%) | Memory: 156.1 MB (20.82%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,15 +2,20 @@ class Solution:
     def rec(self, ind, nums, target):
         if target==0:
             return 1
-        if ind>=len(nums): return 0
+
+        if ind>=len(nums):
+            return 0 
         if (ind, target) in self.dp:
             return self.dp[(ind, target)]
-        not_take=self.rec(ind+1, nums, target)
+
+        notTake=self.rec(ind+1, nums, target)
         take=0
         if nums[ind]<=target:
             take=self.rec(ind, nums, target-nums[ind])
-        self.dp[(ind, target)]= take+not_take 
+        self.dp[(ind, target)]= notTake+take
         return self.dp[(ind, target)]
     def change(self, amount: int, coins: List[int]) -> int:
         self.dp={}
         return self.rec(0, coins, amount)
+        
+        
