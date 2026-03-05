d3b5007be75667db8fd9dbf2af14ab2db36718ce
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-03-05 22:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (32.36%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,24 +4,29 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def rec(self, l1, l2, cur, temp):
-        if l1 is None or l2 is None: 
-            if l1: 
-                cur.next=l1
-            if l2:
-                cur.next=l2
-            return temp.next
-        if l1.val>l2.val: 
-            cur.next=l2
-            return self.rec(l1, l2.next, cur.next, temp)
-        else:
-            cur.next=l1
-            return self.rec(l1.next, l2, cur.next, temp)
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode()
+        ans=dummy
+
+        while list1 and list2:
+            if list1.val>list2.val:
+                node=ListNode(list2.val)
+                ans.next=node 
+                ans=ans.next
+                list2=list2.next 
+            else:
+                node=ListNode(list1.val)
+                ans.next=node
+                ans=ans.next 
+                list1=list1.next 
+
+        if list1:
+            ans.next=list1
+        
+        if list2:
+            ans.next=list2
+
+        return dummy.next
 
 
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        temp=ListNode(-1)  
-        cur=temp
-        final=self.rec(l1, l2, cur, temp)
-        return final
-
+        
