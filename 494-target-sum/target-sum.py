9d58e65554d57d88f9923c8814cba0da615bcfe1
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-03-16 21:51:43 +0530

    Time: 183 ms (19.88%) | Memory: 20.2 MB (45.26%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,18 +1,20 @@
 class Solution:
-    def rec(self, ind, nums,target, sm):
-        if ind>=len(nums) and sm==target:
+    def rec(self, ind, nums, target):
+        if ind>=len(nums) and target==0:
             return 1
-        if ind>=len(nums): return 0
-        if (ind, sm) in self.dp:
-            return self.dp[(ind, sm)]
 
-        add=self.rec(ind+1, nums, target, sm+nums[ind])
-        subtract=self.rec(ind+1, nums, target, sm-nums[ind])
-        self.dp[(ind, sm)]= add+subtract
-        return self.dp[(ind, sm)]
+        if ind>=len(nums):
+            return 0 
+
+        if (ind, target) in self.dp:
+            return self.dp[(ind, target)]
+
+        plus=self.rec(ind+1, nums, target-nums[ind])
+        minus=self.rec(ind+1, nums, target+nums[ind])
+        self.dp[(ind, target)]= plus+minus
+        return self.dp[(ind, target)]
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         self.dp={}
-        return self.rec(0, nums, target, 0)
-        
+        return self.rec(0, nums, target)
         
