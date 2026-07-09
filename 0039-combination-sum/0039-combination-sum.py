173a5a841ebb197ddfe97708c3a11a55e573766f
KajalPathriyal03 <166546396+KajalPathriyal03@users.noreply.github.com>
2026-07-10 01:08:21 +0530

    Time: 19 ms (12.18%), Space: 19.3 MB (91.15%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def backtracking(self, ind, nums, target, res):
+        if ind>=len(nums): return 
+        if target<0: return 
+        if target==0:
+            self.ans.append(res.copy()) 
+            return
+        res.append(nums[ind])
+        self.backtracking(ind, nums, target-nums[ind], res)
+        res.pop()
+        self.backtracking(ind+1, nums, target, res)     
+        
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        self.ans=[]
+        self.backtracking(0, candidates, target, [])
+        return self.ans 
